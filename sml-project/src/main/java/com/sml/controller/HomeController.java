b379d82f0e201843da764ee24b9e49aca3c434f6
Sai Kumar Dash <skd567891@gmail.com>
2026-04-10 17:45:00 +0530

    Create HomeController.java

---
+++ sml-project/src/main/java/com/sml/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.sml.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/signin.html";
+    }
+}
